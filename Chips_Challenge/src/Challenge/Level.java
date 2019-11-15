506f793b3ec0ea0f713af3fa8e5a22778c8a81a5
George Carpenter <gnome.c@protonmail.com>
2019-11-15 18:14:59 +0000

    Level Class sort of has a levelBuilder

--- Chips_Challenge/src/Challenge/Level.java
+++ Chips_Challenge/src/Challenge/Level.java
@@ -1,9 +1,15 @@
 package Challenge;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Level {
 
     private Object[][] cellGrid;
     private Object[][] entityGrid;
+
+    Scanner reader;
 
     public Level() {
 
@@ -13,8 +19,59 @@ public class Level {
 
     }
 
-    public void buildLevel() {
+    public void buildLevel(String level) throws FileNotFoundException {
 
+        reader = new Scanner(new File("Level_Files/" + level + ".txt"));
+        reader.useDelimiter(",");
+
+        int x = reader.nextInt();
+        int y = reader.nextInt();
+
+        Object[][] cellGrid = new Object[x][y];
+        Object[][] entityGrid = new Object[x][y];
+
+        for (int i = 0 ; i < y ; i++) {
+
+            char[] row = reader.nextLine().toCharArray();
+
+            for (int j = 0 ; j < x ; j++) {
+
+                if (row[j] == '#') {
+                    // This is a wall
+                    cellGrid[i][j] = new Wall();
+                } else {
+                    // This is a ground
+                    cellGrid[i][j] = new Ground();
+                }
+
+            }
+        }
+
+        setCellGrid(cellGrid);
+
+        while (reader.hasNextLine()) {
+
+            String thingName = reader.next();
+
+            if (thingName.equals("PLAYER")) {
+                x = reader.nextInt();
+                y = reader.nextInt();
+                entityGrid[x][y] = new Player();
+            }
+
+            // Todo : the rest of these
+
+        }
+
+        setEntityGrid(entityGrid);
+    }
+
+    public void setCellGrid(Object[][] cellGrid) {
+        this.cellGrid = cellGrid;
+    }
+
+    public void setEntityGrid(Object[][] entityGrid) {
+        this.entityGrid = entityGrid;
     }
 
     public int[] getLocation() {
---
+++ Chips_Challenge/src/Challenge/User.java
@@ -0,0 +1,27 @@
+package Challenge;
+
+public class User {
+
+    private String userName;
+    private int[] scores;
+
+    public User() {
+
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setScores(int[] scores) {
+        this.scores = scores;
+    }
+
+    public String getUserName() {
+        return this.userName;
+    }
+
+    public int[] getScores() {
+        return this.scores;
+    }
+}
