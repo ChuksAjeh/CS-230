4b5086d7108a733c18cb7cfb220792b05455e41a
George Carpenter <gnome.c@protonmail.com>
2019-11-19 21:21:18 +0000

    Wall Enemy cleaned up

--- Chips_Challenge/src/Challenge/WallEnemy.java
+++ Chips_Challenge/src/Challenge/WallEnemy.java
@@ -13,7 +13,7 @@ public class WallEnemy extends Enemy {
 
     private static final EntityType entityType;
     private static final Image sprite;
-    private Random random = new Random();
+    private Random random;
 
     static {
         entityType = EntityType.WALL_ENEMY;
@@ -22,6 +22,7 @@ public class WallEnemy extends Enemy {
 
     public WallEnemy(int direction) {
         super(entityType, sprite, direction);
+        random = new Random();
     }
 
     private int nextDirection() {
@@ -37,75 +38,84 @@ public class WallEnemy extends Enemy {
         // 4) For 0-2 IF both LEFT(3) and RIGHT(1) are GROUND then do random between LEFT(3) and RIGHT(1)
         // 5) For 1-3 IF both UP(0) and DOWN(2) are GROUND then do random between UP(0) and DOWN(2)
 
-        if (0 == this.getDirection() || 2 == this.getDirection()) {
-            // UP and DOWN
-            if (Cell.CellType.WALL == this.getCellGrid()[this.getEnemyX()-1][this.getEnemyY()].getCellType()
-                && Cell.CellType.WALL == this.getCellGrid()[this.getEnemyX()+1][this.getEnemyY()].getCellType()){
+        int direction = this.getDirection();
 
-                if(Cell.CellType.WALL == this.getCellGrid()[this.getEnemyX()][this.getEnemyY()-1].getCellType()){
+        Cell.CellType up = this.getCellGrid()[this.getEnemyX()][this.getEnemyY() -1].getCellType();
+        Cell.CellType right = this.getCellGrid()[this.getEnemyX() + 1][this.getEnemyY()].getCellType();
+        Cell.CellType down = this.getCellGrid()[this.getEnemyX()][this.getEnemyY() + 1].getCellType();
+        Cell.CellType left = this.getCellGrid()[this.getEnemyX() - 1][this.getEnemyY()].getCellType();
+
+        if (0 == direction || 2 == direction) {
+
+            // Up + Down
+
+            if (Cell.CellType.WALL == left && Cell.CellType.WALL == right) {
+
+                if (Cell.CellType.WALL == up) {
                     return 2;
-                }
-
-                else if(Cell.CellType.WALL == this.getCellGrid()[this.getEnemyX()][this.getEnemyY()+1].getCellType()){
+                } else if (Cell.CellType.WALL == down) {
                     return 0;
+                } else {
+                    return direction;
                 }
 
-                else{
-                    if (0 == this.getDirection()){
-                        return 0;
-                    }
-                    else{
-                        return 2;
-                    }
-                }
-            }
-            else {
-                if (Cell.CellType.WALL == this.getCellGrid()[this.getEnemyX()-1][this.getEnemyY()].getCellType()){
+            } else if (0 == direction && Cell.CellType.WALL == up) {
+
+                if (Cell.CellType.WALL == left) {
                     return 1;
-                }
-                else if (Cell.CellType.WALL == this.getCellGrid()[this.getEnemyX()+1][this.getEnemyY()].getCellType()){
-                    return 3;
-                }
-                else {
-                    return this.random.nextBoolean() ? 1 : 3;
-                }
-            }
-        }
-        else if (1 == this.getDirection() || 3 == this.getDirection()) {
-            // RIGHT AND LEFT
-            if (Cell.CellType.WALL == this.getCellGrid()[this.getEnemyX()][this.getEnemyY()-1].getCellType()
-                    && Cell.CellType.WALL == this.getCellGrid()[this.getEnemyX()+1][this.getEnemyY()+1].getCellType()){
-
-                if(Cell.CellType.WALL == this.getCellGrid()[this.getEnemyX()-1][this.getEnemyY()].getCellType()){
-                    return 1;
-                }
-
-                else if(Cell.CellType.WALL == this.getCellGrid()[this.getEnemyX()+1][this.getEnemyY()].getCellType()){
+                } else if (Cell.CellType.WALL == right) {
                     return 3;
                 }
 
-                else{
-                    if (1 == this.getDirection()){
-                        return 1;
-                    }
-                    else{
-                        return 3;
-                    }
+            } else if (2 == direction && Cell.CellType.WALL == down) {
+
+                if (Cell.CellType.WALL == left) {
+                    return 1;
+                } else if (Cell.CellType.WALL == right) {
+                    return 3;
                 }
 
+            } else {
+                return direction;
             }
-            else {
-                if (Cell.CellType.WALL == this.getCellGrid()[this.getEnemyX()][this.getEnemyY()-1].getCellType()){
+
+        } else if (1 == direction || 3 == direction) {
+
+            // Left + Right
+
+            if (Cell.CellType.WALL == up && Cell.CellType.WALL == down) {
+
+                if (Cell.CellType.WALL == left) {
+                    return 1;
+                } else if (Cell.CellType.WALL == right) {
+                    return 3;
+                } else {
+                    return direction;
+                }
+
+            } else if (1 == direction && Cell.CellType.WALL == right) {
+
+                if (Cell.CellType.WALL == up) {
+                    return 2;
+                } else if (Cell.CellType.WALL == down) {
                     return 0;
                 }
-                else if (Cell.CellType.WALL == this.getCellGrid()[this.getEnemyX()][this.getEnemyY()+1].getCellType()){
+
+            } else if (3 == direction && Cell.CellType.WALL == left) {
+
+                if (Cell.CellType.WALL == up) {
                     return 2;
+                } else if (Cell.CellType.WALL == down) {
+                    return 0;
                 }
-                else {
-                    return this.random.nextBoolean() ? 0 : 2;
-                }
+
+            } else {
+                return direction;
             }
+
         }
+
         return 0;
+
     }
 }
